ee4dd036579f902816d08497c4d275638b05bc0e
caninemwenja <mwenja07@gmail.com>
2014-01-29 08:35:24 +0300

    Make testing grammars more generic and extensible
    using GrammarTest class to abstract over grammar testing. Look at
    test_np.py to find out how it's used.

---
+++ tests/generic.py
@@ -0,0 +1,30 @@
+from parser_tool import parse, get_parser
+from utils import go_over_file
+
+class GrammarTest(object):
+    
+    def __init__(self, grammar, positive, negative):
+        self.grammar = grammar
+        self.positive = positive
+        self.negative = negative
+
+        self.parser = get_parser(self.grammar, trace=0)
+    
+    def trees(self, sentence):
+        return parse(self.parser, sentence)
+
+    def is_ok(self, sentence):
+        assert len(self.trees(sentence)) > 0, \
+            "Failed Positive: %s" % sentence
+
+    def is_not_ok(self, sentence):
+        assert len(self.trees(sentence)) == 0, \
+            "Failed negative: %s" % sentence
+
+    def check_positive(self):
+        go_over_file(self.positive, self.is_ok)
+
+    def check_negative(self):
+        go_over_file(self.negative, self.is_not_ok)
+
+
--- tests/test_np.py
+++ tests/test_np.py
@@ -1,21 +1,9 @@
-from parser_tool import parse, get_parser
-from utils import go_over_file
+from .generic import GrammarTest
 
-grammar = get_parser("grammars/test_np.fcfg", trace=0)
-
-def test_np_positive():
-    def is_ok(sentence):
-        trees = parse(grammar, sentence)
-        assert len(trees) > 0, "Failed Positive: %s" % sentence
-    
-    go_over_file("grammars/nounphrase.sample", is_ok)
-
-def test_np_negative():
-    """ tests to see if grammar refuses wrong samples """
-    
-    def is_not_ok(sentence):
-        trees = parse(grammar, sentence)
-        assert len(trees) == 0, "Failed Negative: %s" % sentence
-
-    go_over_file("grammars/nounphrase.sample.negative", is_not_ok)
+def test_np():
+    grammar = GrammarTest('grammars/test_np.fcfg',
+        'grammars/nounphrase.sample', 
+        'grammars/nounphrase.sample.negative')
+    grammar.check_positive()
+    grammar.check_negative()
 
