3cdbba23360705ed57179185b372f65735fcaae8
caninemwenja <mwenja07@gmail.com>
2014-01-29 08:45:08 +0300

    Testing infrastructure for subject grammar
    Added a noun phrase line to the positive samples as an example

--- tests/test_grammars.py
+++ tests/test_grammars.py
@@ -7,3 +7,10 @@ def test_np():
     grammar.check_positive()
     grammar.check_negative()
 
+def test_subject():
+    grammar = GrammarTest('grammars/test_subject.fcfg',
+        'grammars/subjectphrase.sample',
+        'grammars/subjectphrase.sample.negative')
+    grammar.check_positive()
+    grammar.check_negative()
+
